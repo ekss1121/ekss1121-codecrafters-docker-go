dc3531c1a7cc2de5a5c8eb3ff614afb7f10e9aec
Zhihang Liu <ekss1121@gmail.com>
2023-02-13 23:56:10 -0500

    pass stage 4

--- app/main.go
+++ app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
+	"path/filepath"
+	"syscall"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
@@ -19,20 +21,46 @@ func main() {
 	//
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	// create a temporary directory to chroot into
+	chrootDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(chrootDir)
+
+	// copy the binary to run
+	targetPath := filepath.Join(chrootDir, command)
+	// fmt.Println("target path is ", targetPath)
+	err = os.MkdirAll(filepath.Dir(targetPath), os.ModePerm)
+
+	if err != nil {
+		panic(err)
+	}
+
+	bytesRead, err := ioutil.ReadFile(command)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(targetPath, bytesRead, os.ModePerm)
+
+	err = syscall.Chroot(chrootDir)
+	if err != nil {
+		panic(err)
+	}
+
 	cmd := exec.Command(command, args...)
-	if args[0] == "exit" {
-		exitCode, err := strconv.Atoi(args[1])
-		if err != nil {
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	err = cmd.Run()
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			os.Exit(ee.ExitCode())
+		} else {
 			fmt.Printf("Err: %v\n", err)
 			os.Exit(1)
 		}
-		os.Exit(exitCode)
-	}
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Err: %v\n", err)
-		os.Exit(1)
 	}
 }
